307228ffcd9e3b517d9f18f9ef7cc31cd033a0cb
Dung <vietdung.rx@gmail.com>
2020-05-10 22:54:55 +0700

    update file

--- index.php
+++ index.php
@@ -6,6 +6,31 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        h1{
+            color: navy;
+        }
+
+        label{
+            width: 10em;
+            padding-right: 1em;
+            float: left;
+        }
+
+        #data input{
+            float: left;
+            width: 15em;
+            margin-bottom: .5em;
+        }
+
+        #buttons input{
+            float: left;
+            margin-bottom: .5em;
+        }
+        br{
+            clear: left;
+        }
+    </style>
 </head>
 <body>
 <h1>Future Value Calculator</h1>
